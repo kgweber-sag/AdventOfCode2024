444ec1e9b0ecf2f57f6e538d78b07aec996133ef
kgweber <arfenarf@gmail.com>
2024-12-03 10:29:06 -0500

    formatting

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,5 +1,6 @@
 import re
 
+
 class MemoryUncorruptor:
     def __init__(self, file_path, consider_dos=False):
         self.data = self._read_file(file_path)
@@ -14,15 +15,15 @@ class MemoryUncorruptor:
 
     def _locate_valid_operations(self):
         for line in self.data:
-            self.operations.extend( re.findall(r'((?:(mul)\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don\'t\(\)))', line))
+            self.operations.extend(re.findall(r'((?:(mul)\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don\'t\(\)))', line))
 
     def execute_operations(self):
-        operation_sum= 0
+        operation_sum = 0
         self._locate_valid_operations()
         for operation in self.operations:
+
             if not operation[0]:  # Skip empty matches
                 continue
-
             if operation[0] == 'do()':
                 self.enabled = True
             elif operation[0] == "don't()":
@@ -34,6 +35,7 @@ class MemoryUncorruptor:
                 operation_sum += x * y
 
         return operation_sum
+
 
 if __name__ == '__main__':
     mu = MemoryUncorruptor('input_d03.txt', consider_dos=False)
