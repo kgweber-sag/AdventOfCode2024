e1e80544e8fa36df3f8fd2539389dfe9ef0d5d3d
kgweber <arfenarf@gmail.com>
2024-12-04 15:36:35 -0500

    p2 doesn't work, start rethink

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -1,54 +1,77 @@
 import numpy as np
+import re
+from typing import List, Tuple
 
-class FindXMAS:
+
+class XMASFinder:
+    DIRECTIONS = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if not (i == 0 and j == 0)]
+    TARGET = 'XMAS'
+
     def __init__(self, file_path):
-        self.xmas = 'XMAS'
-        self.data = self._read_file(file_path)
-        self.rows = len(self.data)
-        self.cols = len(self.data[0])
-        self.xmas_found = 0
+        self.grid, self.bfstring = self.read_data(file_path)
+        self.rows, self.cols = self.grid.shape
 
-    def _read_file(self, file_path):
-        with open(file_path, 'r') as file:
-            lines = [[x for x in line.strip()] for line in file]
+    def read_data(self, file_path: str):
+        with open(file_path) as f:
+            lines = [x.strip() for x in f.readlines()]
+        grid = np.array([[x for x in line] for line in lines])
+        bigfatstring = ''.join(lines)
+        return grid, bigfatstring
 
-        return np.array(lines)
+    def is_valid_pos(self, row: int, col: int) -> bool:
+        return 0 <= row < self.rows and 0 <= col < self.cols
 
-    def _check_xmas(self, r, c):
+    def check_direction(self, start_row: int, start_col: int, delta_row: int, delta_col: int) -> bool:
+        for idx, letter in enumerate(self.TARGET):
+            row = start_row + delta_row * idx
+            col = start_col + delta_col * idx
+            if not self.is_valid_pos(row, col) or self.grid[row, col] != letter:
+                return False
+        return True
 
-        check_letters =[x for x in self.xmas]
+    def count_paths(self) -> int:
+        count = 0
+        x_positions = np.argwhere(self.grid == 'X')
 
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == 0 and j == 0:
-                    continue
-                check_row = r + i
-                check_col = c + j
-                if 0 <= check_row < self.rows and 0 <= check_col < self.cols:
-                    if self.data[check_row ][check_col ] == check_letters[1]:
-                        # we know our direction change is the i, j vector
-                        for index, letter in enumerate(check_letters[2:]):
-                            row_delta = i*(index+2)
-                            col_delta = j*(index+2)
-                            if 0 <= (r + row_delta) < self.rows and 0 <= (c + col_delta) < self.cols:
-                                if self.data[r + row_delta][c + col_delta] == letter:
-                                    if index+3 == len(self.xmas):
-                                        self.xmas_found += 1
-                                    else:
-                                        continue
-                                else:
-                                    break
+        for row, col in x_positions:
+            for delta_row, delta_col in self.DIRECTIONS:
+                if self.check_direction(row, col, delta_row, delta_col):
+                    count += 1
 
+        return count
 
-    def find_xmas(self):
-        for r in range(self.rows):
-            for c in range(self.cols):
-                if self.data[r][c] == self.xmas[0]:
-                    self._check_xmas(r, c)
+    def check_a_mas(self, expr):
+        start = 0
+        while start < len(self.bfstring) + self.cols + 1:
+                match = re.search(expr, self.bfstring[start:])
+                if match:
+                    self.matches += 1
+                    if np.mod(match.start() + 4, self.cols) == 0:
+                        start += match.start() + 4
+                    else:
+                        start += match.start() + 1
+                    print(match)
+                else:
+                    break
 
+    def find_masmas(self):
+
+        print(self.bfstring)
+        exprs = [r'(M.S).{'+str(self.cols-3)+'}(.A.).{'+str(self.cols-3)+'}(M.S)',
+                 r'(S.M).{'+str(self.cols-3)+'}(.A.).{'+str(self.cols-3)+'}(S.M)',
+                 r'(M.M).{'+str(self.cols-3)+'}(.A.).{'+str(self.cols-3)+'}(S.S)',
+                 r'(S.S).{'+str(self.cols-3)+'}(.A.).{'+str(self.cols-3)+'}(M.M)']
+
+        self.matches = 0
+
+        for expr in exprs:
+            self.check_a_mas(expr)
+
+        return self.matches
 
 
 if __name__ == '__main__':
-    fx = FindXMAS('input_d04.txt')
-    fx.find_xmas()
-    print(fx.xmas_found)
+    xf = XMASFinder('input_d04_test.txt')
+    # paths =  XMASFinder(grid).count_paths()
+    # print(paths)
+    print(xf.find_masmas())
